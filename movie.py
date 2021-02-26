5148737c4f0d7c12ebbce9b74eca2509f98708b0
mitaharumi <mitaharumi@gmail.com>
2021-02-26 10:01:42 -0300

    complete endpoint path; change idMovie; fix comments

--- movie.py
+++ movie.py
@@ -1,37 +1,49 @@
-from access import apiKey
+from access import apiKeyTheMovieDataBase
 import requests
 
-APIKEY = apiKey()
+apiKey = apiKeyTheMovieDataBase()
 
-# HTTP requests METHODS
-"""
-GET -> grab data  
-POST -> send data -> add/update data 
-
-PATCH 
-PUT 
-DELETE  
-"""
-# endpoint
-#   >> likes: where is where we want request (url)
-
-# HTTP method
-#   >> what http we need? <GET> <POST> <DEL> <PUT>
 
 """
-endpoint: 
-GET
-/movie/{movie_id}
-
-https://api.themoviedb.org/3/movie/550?api_key=e56a23d5fd1faaf6121235a3c0ba9052
+    # HTTP requests METHODS
+    
+    GET -> grab data  
+    POST -> send data -> add/update data 
+    
+    PATCH 
+    PUT 
+    DELETE  
+    
+    # endpoint
+    #   >> likes: where is where we want request (url)
+    
+    # HTTP method
+    #   >> what http we need? <GET> <POST> <DEL> <PUT>
+    
+    # example
+    endpoint: 
+    GET
+    /movie/{movie_id}
+    
+    https://api.themoviedb.org/3/movie/550?api_key=e56a23d5fd1faaf6121235a3c0ba9052
+    {protocol}    {domain}{version}{movie}{moveId}?api_key={apiKey} 
+    
 """
-
-movieId = [550]
+movieId = '808-shrek'
 apiVersion = 3
 apiBaseUrl = f'https://api.themoviedb.org/{apiVersion}'
-endpointPath = f'/movie/{movieId}'
-endpoint = f'{apiBaseUrl}{endpointPath}'
 
-r = requests.get(endpoint, data={'APIKEY': APIKEY})
+endpointPath = f'/movie/{movieId}'
+
+# >> this is works in some APIs ...
+# endpoint = f'{apiBaseUrl}{endpointPath}'
+# r = requests.get(endpoint, data={'api_key': apiKey})
+# r = requests.get(endpoint, json={'api_key': apiKey})
+# <<
+
+endpoint = f'{apiBaseUrl}{endpointPath}?api_key={apiKey}'
+r = requests.get(endpoint)
 print(r.status_code)
 print(r.text)
+
+
